3da8fd77ec754cd90b6f337cd2063af54b41091c
Ryan Fitzpatrick <rmfitzpatrick@signalfx.com>
2018-12-07 16:51:36 -0500

    Update Python example for OT2.0

--- jaeger-python/pi_trace/pi_server.py
+++ jaeger-python/pi_trace/pi_server.py
@@ -4,7 +4,6 @@ import traceback
 import logging
 import json
 
-from opentracing_instrumentation import span_in_context
 from six.moves import BaseHTTPServer as http_server
 from six.moves import socketserver
 from jaeger_client import Config
@@ -14,7 +13,6 @@ import opentracing
 from propagation import propagated_context
 from leibniz import calculate_partial
 
-
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level='DEBUG')
 
 
@@ -22,6 +20,7 @@ class PiHandler(http_server.BaseHTTPRequestHandler):
     """An instrumented request handler that calculates partial pi approximations
     based on the "term_start" and "term_end" json field values of the POST request body
     """
+
     def do_POST(self):
         # Get previously initialized tracer or a noop tracer if not globally initialized.
         # Noop tracer provides noop tracer and span implementations for all high-level interfaces.
@@ -40,8 +39,9 @@ class PiHandler(http_server.BaseHTTPRequestHandler):
                      tags.HTTP_METHOD: 'POST',
                      tags.PEER_ADDRESS: ':'.join(map(str, self.client_address))}
 
-        # Opentracing spans provide helpful context manager for transparent span scoping
-        with tracer.start_span('post_handler', child_of=span_ctx, tags=span_tags) as span:
+        # Opentracing spans provide helpful context scope manager for transparent span scoping
+        with tracer.start_active_span('post_handler', child_of=span_ctx, tags=span_tags) as scope:
+            span = scope.span  # obtain the span from the active scope
             # Set request-level tags
             span.set_tag('post_handler.headers', json.dumps(headers))
 
@@ -53,12 +53,7 @@ class PiHandler(http_server.BaseHTTPRequestHandler):
 
                 # Log events for annotation creation in SignalFx
                 span.log_kv(dict(event='calculation beginning.'))
-
-                # Provide subsequent, shared thread invocations of
-                # opentracing_instumentation.get_current_span() with this span
-                with span_in_context(span):
-                    partial_sum = calculate_partial(start, end)
-
+                partial_sum = calculate_partial(start, end)
                 span.log_kv(dict(event='calculation finished.'))
             except Exception as e:
                 span.set_tag(tags.ERROR, True)
@@ -67,7 +62,7 @@ class PiHandler(http_server.BaseHTTPRequestHandler):
                              'error.kind': str(type(e)),
                              'error.object': tb})
                 span.set_tag(tags.HTTP_STATUS_CODE, 500)
-                self.write_response('Error occurred while processing request:\n{}' .format(tb), 500)
+                self.write_response('Error occurred while processing request:\n{}'.format(tb), 500)
             else:
                 span.set_tag(tags.HTTP_STATUS_CODE, 200)
                 self.write_response(str(partial_sum), 200)
@@ -94,22 +89,21 @@ if __name__ == '__main__':
     args = ap.parse_args()
 
     # Establish a jaeger-client-python configuration to report spans to SignalFx
-    config = Config(
-        config={
-            'sampler': {
-                'type': 'const',
-                'param': 1,
-            },
-            'propagation': 'b3',  # Interoperable with Zipkin and Jaeger traces
-            'jaeger_endpoint': args.ingest,
-            'jaeger_format_params': '',  # No format query parameters required by SignalFx
-            'jaeger_user': 'auth',  # Required static username for Access Token authentication via Basic Access
-            'jaeger_password': args.access_token,  # Your organization's Access Token
-            'logging': True,  # Report helpful span submission statements and errors to log handler
-        },
-        service_name='PiServer',  # Service name (low cardinality)
-        validate=True,  # Have jaeger_client fail quickly on invalid configuration
-    )
+    jaeger_config = {
+        'sampler': {'type': 'const',  # Report all spans for analysis by SignalFx Gateway
+                    'param': 1},
+        'propagation': 'b3',  # Interoperable with Zipkin and Jaeger traces
+        'jaeger_endpoint': args.ingest,
+        'logging': True,  # Report helpful span submission statements and errors to log handler
+    }
+    if args.access_token:  # Authorization info not needed for Smart Agent or Gateway
+        # Required static username for organization Access Token authentication via Basic Access
+        jaeger_config['jaeger_user'] = 'auth'
+        jaeger_config['jaeger_password'] = args.access_token
+
+    config = Config(config=jaeger_config,
+                    service_name='PiServer',  # Service name (low cardinality)
+                    validate=True)  # Have jaeger_client fail quickly on invalid configuration
 
     # Globally sets opentracer.tracer to use jaeger_client over default noop tracer
     tracer = config.initialize_tracer()
@@ -119,3 +113,5 @@ if __name__ == '__main__':
         server.serve_forever()
     except KeyboardInterrupt:
         pass
+
+    tracer.close()
