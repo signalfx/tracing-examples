59e6b96c91aa24eeb961c8ee14b7c99cf26a49d9
Ashwin Chandrasekar <achandrasekar@signalfx.com>
2019-01-11 12:11:27 -0500

    Modify handler span directly rather than creating a new one

--- aws-lambda/jaeger-ruby/prime_sum.rb
+++ aws-lambda/jaeger-ruby/prime_sum.rb
@@ -12,20 +12,17 @@ require 'signalfx/lambda'
 
 # this is the original lambda handler
 def handle_request(event:, context:)
-  sum = 0
 
-  # when using the wrapper, the tracer is available through OpenTracing.global_tracer
-  # to do more granular instrumentation
-  OpenTracing.global_tracer.start_active_span("handle_request") do |scope|
+  # the handler span can be accessed as the active span:
+  span = OpenTracing.active_span
 
-    # read the body field from the event and tag the span with it
-    body = event['body']
-    scope.span.set_tag("body", body)
+  # read the body field from the event and tag the span with it
+  body = event['body']
+  span.set_tag("body", body)
 
-    # perform the main work of this function
-    limit = event['queryStringParameters']['limit']
-    sum = prime_sum(limit)
-  end
+  # perform the main work of this function
+  limit = event['queryStringParameters']['limit']
+  sum = prime_sum(limit)
 
   # when using the default API Gateway proxy, the function must return a JSON document with statusCode and body.
   # The requirements may differ if the gateway is configured differently
@@ -39,6 +36,9 @@ SignalFx::Lambda::Tracing.register_handler(&method(:handle_request))
 # sum up the list of primes and tag the span with the total
 def prime_sum(n)
   sum = 0
+
+  # when using the wrapper, the tracer is available through OpenTracing.global_tracer
+  # to do more granular instrumentation
   OpenTracing.global_tracer.start_active_span("calculate_sum") do |scope|
     primes = sieve(n)
     sum = primes.reduce( :+ )
