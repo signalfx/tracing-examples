06bcc4ac656416d6df3803b402bc5a3476548765
jkwatson <jwatson@splunk.com>
2021-02-22 13:26:17 -0800

    Update the kotlin-spring example documentation And update the startup script to match the latest

--- opentelemetry-tracing/opentelemetry-java-tracing/spring-boot-kotlin/start.sh
+++ opentelemetry-tracing/opentelemetry-java-tracing/spring-boot-kotlin/start.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
-export OTEL_ZIPKIN_SERVICE_NAME=wishlist-app
+export OTEL_RESOURCE_ATTRIBUTES=service.name=wishlist-app
+
+## use below to point to a default Jaeger installation on localhost:
+#export OTEL_EXPORTER_JAEGER_ENDPOINT=http://localhost:14268/api/traces
 
 docker-compose -f postgres/docker-compose.yml up -d
 
-mvn spring-boot:run -Dspring-boot.run.jvmArguments="-javaagent:/opt/opentelemetry-javaagent-all.jar"
+mvn spring-boot:run -Dspring-boot.run.jvmArguments="-javaagent:opentelemetry-javaagent-all.jar"
