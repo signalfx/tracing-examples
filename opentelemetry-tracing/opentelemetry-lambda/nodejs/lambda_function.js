4b73e0e30e4a88f637bee9993ed3ec09b797ca18
Jakub Wach <jwach@splunk.com>
2022-03-08 09:32:15 +0100

    New otel lambda Node.js examples (#102)

--- opentelemetry-tracing/opentelemetry-lambda/nodejs/lambda_function.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.lambda_handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
-};
