f8eeb776ccef55873f3f709e71f29d3ccb7126ed
Robert Pająk <pellared@hotmail.com>
2022-04-08 13:23:53 +0200

    Make better cleanup

--- opentelemetry-tracing/opentelemetry-go/gorilla-mux/run.sh
+++ opentelemetry-tracing/opentelemetry-go/gorilla-mux/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+function cleanup {
+  docker-compose -f "${DIR}/docker-compose.yaml" down
+  kill 0 # kill background processes
+}
+trap cleanup EXIT
+
 set -euxo pipefail
 
 export OTEL_RESOURCE_ATTRIBUTES="deployment.environment=$(whoami)"
 docker-compose -f "${DIR}/docker-compose.yaml" build
 docker-compose -f "${DIR}/docker-compose.yaml" up
-docker-compose -f "${DIR}/docker-compose.yaml" down
