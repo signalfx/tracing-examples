66d4794e4146a7a52c6c3e6ed43bf3bf5e84ac1b
konfortes <konfortes@gmail.com>
2022-11-10 16:35:28 +0200

    Update Falcon example to use version 3
    Falcon 2 is rarely used, there's version 4 out there. Change the example to use version 3 of falcon which deprecates the API class in favor of App. The falcon instrumentation package in version 3 only able to inject the middleware when the App class is used.

--- opentelemetry-tracing/opentelemetry-python-tracing/falcon/main.py
+++ opentelemetry-tracing/opentelemetry-python-tracing/falcon/main.py
@@ -1,30 +1,23 @@
 #!/usr/bin/env python
 
 import json
-import sys
-import logging
 
 import falcon
 from wsgiref import simple_server
-from opentelemetry import trace
-from opentelemetry.trace import SpanKind
 
-
-tracer = trace.get_tracer(__name__)
 
 class HelloWorldResource(object):
     def on_get(self, req, resp):
         """Handles GET requests"""
-        with tracer.start_as_current_span('internal_span', kind=SpanKind.INTERNAL) as span:
-            resp.body = json.dumps({'ok': True})
+        resp.body = json.dumps({"ok": True})
 
 
-app = falcon.API()
+app = falcon.App()
 
-app.add_route('/hello', HelloWorldResource())
+app.add_route("/hello", HelloWorldResource())
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = 8000
-    print('starting server: ', port)
-    httpd = simple_server.make_server('127.0.0.1', port, app)
+    print("starting server: ", port)
+    httpd = simple_server.make_server("127.0.0.1", port, app)
     httpd.serve_forever()
