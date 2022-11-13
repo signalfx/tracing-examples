29ce64450e4778012023af1dfb05fb7e7dea20c7
Ronen Konfortes <konfortes@gmail.com>
2022-11-13 10:13:42 +0200

    Revert double quotes

--- opentelemetry-tracing/opentelemetry-python-tracing/falcon/main.py
+++ opentelemetry-tracing/opentelemetry-python-tracing/falcon/main.py
@@ -9,15 +9,15 @@ from wsgiref import simple_server
 class HelloWorldResource(object):
     def on_get(self, req, resp):
         """Handles GET requests"""
-        resp.body = json.dumps({"ok": True})
+        resp.body = json.dumps({'ok': True})
 
 
 app = falcon.App()
 
-app.add_route("/hello", HelloWorldResource())
+app.add_route('/hello', HelloWorldResource())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     port = 8000
-    print("starting server: ", port)
-    httpd = simple_server.make_server("127.0.0.1", port, app)
+    print('starting server: ', port)
+    httpd = simple_server.make_server('127.0.0.1', port, app)
     httpd.serve_forever()
