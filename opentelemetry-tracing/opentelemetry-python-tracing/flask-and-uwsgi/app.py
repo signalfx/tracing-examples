6a9203a33dc132ad5f7ba81102c6038b76bddcfb
Pablo Collins <pabcolli@cisco.com>
2026-06-25 15:07:33 -0400

    Fix Flask uWSGI example for current instrumentation

--- opentelemetry-tracing/opentelemetry-python-tracing/flask-and-uwsgi/app.py
+++ opentelemetry-tracing/opentelemetry-python-tracing/flask-and-uwsgi/app.py
@@ -1,14 +1,6 @@
-import uwsgidecorators
-from splunk_otel.tracing import start_tracing
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from flask import Flask
 
 app = Flask(__name__)
-
-@uwsgidecorators.postfork
-def setup_tracing():
-    start_tracing()
-    FlaskInstrumentor().instrument_app(app)
 
 @app.route('/hello/')
 def hello_world():
@@ -16,4 +8,4 @@ def hello_world():
 
 
 # running with uWSGI:
-# uwsgi --http 127.0.0.1:8000 --wsgi-file app.py --callable app --master --processes 4 --enable-threads
+# opentelemetry-instrument uwsgi --http 127.0.0.1:8000 --wsgi-file app.py --callable app --master --processes 4 --enable-threads
