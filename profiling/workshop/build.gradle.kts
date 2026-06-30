92bbe3653a37f657c36400098eaaa467639e0633
Jason Plumb <jplumb@splunk.com>
2026-06-30 11:49:16 -0700

    Upgrade vulnerable sample dependencies

--- profiling/workshop/build.gradle.kts
+++ profiling/workshop/build.gradle.kts
@@ -14,10 +14,18 @@ tasks.withType<Jar> {
 }
 
 dependencies {
-    implementation("com.sparkjava:spark-core:2.9.3")
+    implementation("com.sparkjava:spark-core:2.9.4")
     implementation("org.slf4j:slf4j-api:1.7.32")
     implementation("org.slf4j:slf4j-simple:1.7.32")
-    implementation("io.opentelemetry:opentelemetry-api:1.0.0")
-    implementation("io.opentelemetry:opentelemetry-extension-annotations:1.0.0")
+    implementation("io.opentelemetry:opentelemetry-api:1.62.0")
+    implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.29.0")
 
+}
+
+configurations.all {
+    resolutionStrategy.eachDependency {
+        if (requested.group == "org.eclipse.jetty" || requested.group == "org.eclipse.jetty.websocket") {
+            useVersion("9.4.57.v20241219")
+        }
+    }
 }
